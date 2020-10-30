c72eb36b17a38b539797a731b3ec66bd40ec02d5
Maryam Tufail <maryamtufail279@gmail.com>
2020-10-30 20:29:10 +0500

    lock-json delete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // Component of this App
 import Header from "./components/Header/Header";
-import Balance from "./components/balance/Balance";
+import Balance from "./components/Balance/Balance";
 import IncomeList from "./components/Transaction/IncomeList";
 import ExpenseList from "./components/Transaction/ExpenseList";
 import AddTransaction from "./components/AddTransaction/AddTransaction";
